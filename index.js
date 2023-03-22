740bca58d769a1b4ee5579e920fe23a0dc63d564
Nathan Nguyen <nathannguyen1058@gmail.com>
2023-03-22 16:15:33 -0700

    starter code

---
+++ index.js
@@ -0,0 +1,11 @@
+// Import the necessary packages
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+// Main function to run the application
+async function main() {
+  // TODO: Add code to prompt the user for input and generate the README
+}
+
+// Invoke the main function
+main();
